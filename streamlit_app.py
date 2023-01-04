6e8dcd2a1729a29c87d49079cdc754aadd5c5816
Peter Lindh Snowflake <petlinsnowflake@gmail.com>
2023-01-04 12:27:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 Raspberry Oak meal')
+streamlit.text('Omega 3 Raspberry Oat meal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
